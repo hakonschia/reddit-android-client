df5a6151cdf0428244223760b79e2258cfa153c3
Håkon Schia <hakonschia5@gmail.com>
2020-08-29 11:03:16 +0200

    Add serialized names to AccessToken

--- app/src/main/java/com/example/hakonsreader/api/model/AccessToken.java
+++ app/src/main/java/com/example/hakonsreader/api/model/AccessToken.java
@@ -1,11 +1,21 @@
 package com.example.hakonsreader.api.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class AccessToken {
 
+    @SerializedName("access_token")
     private String accessToken;
+
+    @SerializedName("token_type")
     private String tokenType;
+
+    @SerializedName("expires_in")
     private String expiresIn;
+
     private String scope;
+
+    @SerializedName("refresh_token")
     private String refreshToken;
 
 }
