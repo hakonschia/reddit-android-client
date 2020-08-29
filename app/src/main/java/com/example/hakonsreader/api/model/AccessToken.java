b6b51c0769808543b4dd46e8de75995da71f144d
Håkon Schia <hakonschia5@gmail.com>
2020-08-29 11:00:43 +0200

    Add fields to AccessToken

--- app/src/main/java/com/example/hakonsreader/api/model/AccessToken.java
+++ app/src/main/java/com/example/hakonsreader/api/model/AccessToken.java
@@ -1,4 +1,11 @@
 package com.example.hakonsreader.api.model;
 
 public class AccessToken {
+
+    private String accessToken;
+    private String tokenType;
+    private String expiresIn;
+    private String scope;
+    private String refreshToken;
+
 }
