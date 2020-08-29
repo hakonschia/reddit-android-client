b606d2fa2313394cd3e57cad36db15da2ed9639a
Håkon Schia <hakonschia5@gmail.com>
2020-08-29 11:06:58 +0200

    Change and rename access token url path

--- app/src/main/java/com/example/hakonsreader/constants/NetworkConstants.java
+++ app/src/main/java/com/example/hakonsreader/constants/NetworkConstants.java
@@ -3,6 +3,6 @@ package com.example.hakonsreader.constants;
 public class NetworkConstants {
 
     public static final String REDDIT_API_URL = "https://reddit.com/api/";
-    public static final String ACCESS_TOKEN_URL = REDDIT_API_URL + "v1/access_token";
+    public static final String ACCESS_TOKEN_PATH = "v1/access_token";
 
 }
