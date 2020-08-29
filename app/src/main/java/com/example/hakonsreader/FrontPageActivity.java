3010905bb06b63d6cb097cd112a28339b57c8ca5
Håkon Schia <hakonschia5@gmail.com>
2020-08-29 14:27:25 +0200

    Add constructor/getters

---
+++ app/src/main/java/com/example/hakonsreader/FrontPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.hakonsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrontPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+    }
+}
