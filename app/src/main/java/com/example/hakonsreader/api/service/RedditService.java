2695937e643300a1d71a539062d3b57d1a84bc90
Håkon Schia <hakonschia5@gmail.com>
2020-08-29 10:59:38 +0200

    Add fields to access token call

--- app/src/main/java/com/example/hakonsreader/api/service/RedditService.java
+++ app/src/main/java/com/example/hakonsreader/api/service/RedditService.java
@@ -1,14 +1,20 @@
 package com.example.hakonsreader.api.service;
 
 import com.example.hakonsreader.api.model.AccessToken;
+import com.example.hakonsreader.constants.NetworkConstants;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
-public class RedditService {
+public interface RedditService {
 
-    @GET
-    public Call<AccessToken> getAccessToken() {
-        
-    }
+    @POST(NetworkConstants.ACCESS_TOKEN_URL)
+    @FormUrlEncoded
+    public Call<AccessToken> getAccessToken(
+            @Field("grant_type") String grantType,
+            @Field("code") String code,
+            @Field("redirect_uri") String redirectUri
+    );
 }
--- app/src/main/java/com/example/hakonsreader/constants/NetworkConstants.java
+++ app/src/main/java/com/example/hakonsreader/constants/NetworkConstants.java
@@ -2,5 +2,7 @@ package com.example.hakonsreader.constants;
 
 public class NetworkConstants {
 
+    public static final String REDDIT_API_URL = "https://reddit.com/api/";
+    public static final String ACCESS_TOKEN_URL = REDDIT_API_URL + "v1/access_token";
 
 }
