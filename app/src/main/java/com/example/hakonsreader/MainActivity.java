8efb566774eefc8ba0b8102ec6b15ca04247cefa
Håkon Schia <hakonschia5@gmail.com>
2020-08-29 11:27:50 +0200

    Add internet permissions

--- app/src/main/java/com/example/hakonsreader/MainActivity.java
+++ app/src/main/java/com/example/hakonsreader/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         Uri uri = getIntent().getData();
-
         if (uri == null) {
             return;
         }
