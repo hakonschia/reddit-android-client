6c5195d127a6d5b677cf20306f866c8904e307f4
Håkon Schia <hakonschia5@gmail.com>
2020-09-03 17:13:32 +0200

    Change settings fragment to default created by AS

--- app/src/main/java/com/example/hakonsreader/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/hakonsreader/fragments/SettingsFragment.java
@@ -1,20 +1,15 @@
 package com.example.hakonsreader.fragments;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends Fragment {
+import com.example.hakonsreader.R;
 
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
     }
 }
