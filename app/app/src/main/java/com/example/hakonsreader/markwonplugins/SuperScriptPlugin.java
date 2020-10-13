1f927e3409ca8699be80592606d80210041b5a5d
Håkon Schia <hakonschia5@gmail.com>
2020-10-13 11:16:17 +0200

    Fix superscripts so nested supercripts and superscripts next to each other work as expected

--- app/app/src/main/java/com/example/hakonsreader/markwonplugins/SuperScriptPlugin.java
+++ app/app/src/main/java/com/example/hakonsreader/markwonplugins/SuperScriptPlugin.java
@@ -1,8 +1,8 @@
 package com.example.hakonsreader.markwonplugins;
 
 import android.text.Spannable;
-import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
 import android.text.style.SuperscriptSpan;
 import android.util.Log;
 import android.widget.TextView;
@@ -20,9 +20,12 @@ import io.noties.markwon.AbstractMarkwonPlugin;
 public class SuperScriptPlugin extends AbstractMarkwonPlugin {
     private static final String TAG = "SuperScriptPlugin";
 
-    // TODO this doesn't allow for nesting, so nested superscripts dont work
-    // Subreddits are alphanumericals, numbers, and underscores. Users are the same and dashes
-    public static final Pattern RE = Pattern.compile("[\\^][(].*[)]");
+    // TODO apparently superscripts are either ^() or ^
+
+    // The syntax is ^(), so match starting with ^(, and any character not ) and then a )
+    // We need to match anything not a ) inside to avoid multiple in a row being treated as one
+    // ie. ^(hello) ^(there) but it ruins recursive superscripts arghhhh
+    public static final Pattern RE = Pattern.compile("\\^\\([^)]+\\)");
 
     @Override
     public void beforeSetText(@NonNull TextView textView, @NonNull Spanned markdown) {
@@ -30,8 +33,7 @@ public class SuperScriptPlugin extends AbstractMarkwonPlugin {
     }
 
     private void applySuperSriptSpan(@NonNull Spannable spannable, int start, int end) {
-        final String original = spannable.toString();
-        final String text = original.substring(start, end);
+        final String text = spannable.toString().substring(start, end);
         final Matcher matcher = RE.matcher(text);
 
         while (matcher.find()) {
@@ -40,17 +42,29 @@ public class SuperScriptPlugin extends AbstractMarkwonPlugin {
 
             // Apply the superspan recursively as a superspan can have a superspan inside of it
             // Ie. ^(hello ^(there))
-            applySuperSriptSpan(spannable, s + 2, e - 1);
+            applySuperSriptSpan(spannable, s + 2, e);
 
             int spanStart = s + start;
             int spanEnd = e + start;
 
+            // SuperscriptSpan puts it higher, and RelativeSizeSpan makes the text smaller
             spannable.setSpan(new SuperscriptSpan(), spanStart, spanEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spannable.setSpan(new RelativeSizeSpan(0.85f), spanStart, spanEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
 
             // Hide the original syntax
             // The syntax is ^(...), so hide 2 characters at the start and one at the end
             spannable.setSpan(new HideSyntaxSpan(), spanStart, spanStart + 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            spannable.setSpan(new HideSyntaxSpan(), spanEnd - 1, spanEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            // This is super hacky and I don't even know why this works, but for nested superscripts it doesn't match completely
+            // and it would not hide the last ) unless we go to spanEnd + 1, but if we are at the end we obviously can't
+            // go past the end
+            if (end == spannable.length()) {
+                spannable.setSpan(new HideSyntaxSpan(), spanEnd - 1, spanEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            } else {
+                spannable.setSpan(new HideSyntaxSpan(), spanEnd - 1, spanEnd + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+
         }
     }
 }
