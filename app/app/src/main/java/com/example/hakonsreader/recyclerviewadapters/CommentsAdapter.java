dc73de3a695acf7d53ffd7fbdd1f3b3206218e16
Håkon Schia <hakonschia5@gmail.com>
2020-10-19 21:16:57 +0200

    Call executePendingBindings when binding comments to avoid UI hickups

--- app/app/src/main/java/com/example/hakonsreader/recyclerviewadapters/CommentsAdapter.java
+++ app/app/src/main/java/com/example/hakonsreader/recyclerviewadapters/CommentsAdapter.java
@@ -501,6 +501,7 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.ViewHo
             binding.setComment(comment);
             binding.setIsMoreComments(Thing.MORE.getValue().equals(comment.getKind()));
             binding.setCommentHidden(commentsHidden.contains(comment));
+            binding.executePendingBindings();
 
             // TODO when the vote bar uses data binding this can probably be set through xml
             binding.commentVoteBar.setListing(comment);
