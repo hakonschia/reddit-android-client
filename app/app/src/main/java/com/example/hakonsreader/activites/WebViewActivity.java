be7b1673e8709636c2a5544148db7054ce4cb753
Håkon Schia <hakonschia5@gmail.com>
2020-11-12 19:54:59 +0100

    Add swipe refresh to WebView, add scrolling behaviour to hide toolbar in WebView

--- app/app/src/main/java/com/example/hakonsreader/activites/WebViewActivity.java
+++ app/app/src/main/java/com/example/hakonsreader/activites/WebViewActivity.java
@@ -6,14 +6,18 @@ import android.util.Log;
 import android.webkit.WebBackForwardList;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.hakonsreader.R;
+import com.example.hakonsreader.databinding.ActivityWebViewBinding;
 
 public class WebViewActivity extends AppCompatActivity {
     private static final String TAG = "WebViewActivity";
@@ -23,49 +27,59 @@ public class WebViewActivity extends AppCompatActivity {
      */
     public static final String URL_KEY = "url";
 
+    private ActivityWebViewBinding binding;
 
-    private WebView webView;
-    private TextView urlToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_view);
+        binding = ActivityWebViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         String url = getIntent().getExtras().getString(URL_KEY);
 
-        webView = findViewById(R.id.webView);
-        urlToolbar = findViewById(R.id.webViewUrl);
-
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient() {
+        binding.webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 Uri uri = request.getUrl();
-                urlToolbar.setText(uri.toString());
+                binding.webViewUrl.setText(uri.toString());
                 return super.shouldOverrideUrlLoading(view, request);
             }
-        });
-        webView.getSettings().setJavaScriptEnabled(true);
-        // Some websites wont load without this enabled (such as imgur albums)
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.loadUrl(url);
-        urlToolbar.setText(url);
 
-        findViewById(R.id.webViewClose).setOnClickListener(v -> finish());
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                binding.refreshWebView.setRefreshing(false);
+            }
+        });
+
+        WebSettings webViewSettings = binding.webView.getSettings();
+        webViewSettings.setJavaScriptEnabled(true);
+        // Some websites wont load without this enabled (such as imgur albums)
+        webViewSettings.setDomStorageEnabled(true);
+        binding.webView.loadUrl(url);
+        binding.webViewUrl.setText(url);
+
+        // Close the web view (ie. finish the activity) with the button in the toolbar
+        binding.webViewClose.setOnClickListener(v -> finish());
+
+        binding.refreshWebView.setProgressBackgroundColorSchemeColor(ContextCompat.getColor(this, R.color.colorAccent));
+        binding.refreshWebView.setOnRefreshListener(() -> {
+            binding.webView.reload();
+        });
     }
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
+        if (binding.webView.canGoBack()) {
             // shouldOverrideUrlLoading isn't called when going back, so update the URL here
-            WebBackForwardList mWebBackForwardList = webView.copyBackForwardList();
+            WebBackForwardList mWebBackForwardList = binding.webView.copyBackForwardList();
             if (mWebBackForwardList.getCurrentIndex() > 0) {
                 String url = mWebBackForwardList.getItemAtIndex(mWebBackForwardList.getCurrentIndex() - 1).getUrl();
-                urlToolbar.setText(url);
+                binding.webViewUrl.setText(url);
             }
 
-            webView.goBack();
+            binding.webView.goBack();
         } else {
             super.onBackPressed();
         }
