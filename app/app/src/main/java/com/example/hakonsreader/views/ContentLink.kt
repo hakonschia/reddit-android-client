8517c37d50231a662413ffc8676d84cba628a8fb
Håkon Schia <hakonschia5@gmail.com>
2020-11-21 10:21:51 +0100

    Add new layout for link contents that uses a smaller thumbnail to save data

--- app/app/src/main/java/com/example/hakonsreader/views/ContentLink.kt
+++ app/app/src/main/java/com/example/hakonsreader/views/ContentLink.kt
@@ -5,34 +5,63 @@ import android.content.Intent
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.viewbinding.ViewBinding
+import com.example.hakonsreader.App
 import com.example.hakonsreader.R
 import com.example.hakonsreader.activites.DispatcherActivity
 import com.example.hakonsreader.api.model.Image
+import com.example.hakonsreader.databinding.ContentGalleryBinding
 import com.example.hakonsreader.databinding.ContentLinkBinding
+import com.example.hakonsreader.databinding.ContentLinkSimpleBinding
 import com.squareup.picasso.Picasso
 
 class ContentLink : Content {
 
-    val binding: ContentLinkBinding
+    val binding: ViewBinding
 
     constructor(context: Context?) : super(context) {
-        binding = ContentLinkBinding.inflate(LayoutInflater.from(context), this, true)
+        binding = getCorrectBinding()
     }
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
-        binding = ContentLinkBinding.inflate(LayoutInflater.from(context), this, true)
+        binding = getCorrectBinding()
     }
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        binding = ContentLinkBinding.inflate(LayoutInflater.from(context), this, true)
+        binding = getCorrectBinding()
     }
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
-        binding = ContentLinkBinding.inflate(LayoutInflater.from(context), this, true)
+         binding = getCorrectBinding()
     }
 
-    override fun updateView() {
-        // The previews will (I believe) never be above 1080p, and that should be fine for most devices
-        // TODO although this will use more data, so it might be reasonable to add a data saving setting where
-        //  this image quality is reduced
+    /**
+     * Retrieves and inflates the correct binding to use for the layout based on the users
+     * data saving setting
+     */
+    private fun getCorrectBinding() : ViewBinding {
+        // TODO create and get the actual data saving setting
+        return if (App.get().dontCacheNSFW()) {
+            ContentLinkBinding.inflate(LayoutInflater.from(context), this, true)
+        } else {
+            ContentLinkSimpleBinding.inflate(LayoutInflater.from(context), this, true)
+        }
+    }
 
+
+    override fun updateView() {
+        if (binding is ContentLinkBinding) {
+            this.updateViewNormal()
+        } else {
+            this.updateViewSimple()
+        }
+    }
+
+    /**
+     * Updates the view for the normal
+     */
+    private fun updateViewNormal() {
+        binding as ContentLinkBinding
+
+        // The previews will (I believe) never be above 1080p, and that should be fine for most devices
         var previews: List<Image> = redditPost.previewImages
 
         if (redditPost.isNsfw) {
@@ -54,6 +83,23 @@ class ContentLink : Content {
             val params: ViewGroup.LayoutParams = binding.thumbnail.layoutParams
             params.height = resources.getDimension(R.dimen.contentLinkNoThumbnailSize).toInt()
             binding.thumbnail.layoutParams = params
+        }
+
+        binding.link.text = redditPost.url
+    }
+
+    /**
+     * Updates the view for a simple view. This view uses only the thumbnail for the post, which
+     * will be a smaller image and use less data
+     */
+    private fun updateViewSimple() {
+        binding as ContentLinkSimpleBinding
+
+        val thumbnail = redditPost.thumbnail
+        if (thumbnail.isNotBlank()) {
+            Picasso.get()
+                    .load(thumbnail)
+                    .into(binding.thumbnail)
         }
 
         binding.link.text = redditPost.url
