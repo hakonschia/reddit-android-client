abf3f53f72b223504cfd45ed47489ef872f87c31
Håkon Schia <hakonschia5@gmail.com>
2020-11-25 11:48:53 +0100

    Add Kotlin version of ModRequest

---
+++ app/app/src/main/java/com/example/hakonsreader/api/requestmodels/ModRequestModelKt.kt
@@ -0,0 +1,119 @@
+package com.example.hakonsreader.api.requestmodels
+
+import com.example.hakonsreader.api.RedditApi
+import com.example.hakonsreader.api.enums.Thing
+import com.example.hakonsreader.api.exceptions.InvalidAccessTokenException
+import com.example.hakonsreader.api.model.AccessToken
+import com.example.hakonsreader.api.model.RedditComment
+import com.example.hakonsreader.api.model.RedditPost
+import com.example.hakonsreader.api.responses.ApiResponse
+import com.example.hakonsreader.api.responses.GenericError
+import com.example.hakonsreader.api.service.ModServiceKt
+import com.example.hakonsreader.api.utils.Util
+import com.example.hakonsreader.api.utils.apiError
+import java.lang.Exception
+
+class ModRequestModelKt(
+        private val accessToken: AccessToken,
+        private val api: ModServiceKt
+) {
+
+
+    /**
+     * Distinguish a comment as mod, and optionally sticky it
+     *
+     * @param id The ID of the comment to distinguish
+     * @param distinguish True to distinguish as mod, false to remove previous distinguish
+     * @param sticky True to also sticky the comment. This is only possible on top-level comments
+     */
+    suspend fun distinguishAsModComment(id: String, distinguish: Boolean, sticky: Boolean) : ApiResponse<RedditComment> {
+        try {
+            Util.verifyLoggedInToken(accessToken)
+        } catch (e: InvalidAccessTokenException) {
+            return ApiResponse.Error(GenericError(-1), InvalidAccessTokenException("Distinguishing/stickying a comment requires a valid access token for a logged in user", e))
+        }
+
+        return try {
+            val resp = api.distinguishAsModComment(
+                    Util.createFullName(Thing.COMMENT, id),
+                    if (distinguish) "yes" else "no",
+                    sticky,
+                    RedditApi.API_TYPE
+            )
+
+            val comment = resp.body()?.response?.getListings()?.get(0)
+            if (comment != null) {
+                ApiResponse.Success(comment)
+            } else {
+                apiError(resp)
+            }
+        } catch (e: Exception) {
+            ApiResponse.Error(GenericError(-1), e)
+        }
+    }
+
+    /**
+     * Distinguish a post as mod
+     *
+     * @param id The ID of the post to distinguish
+     * @param distinguish True to distinguish as mod, false to remove previous distinguish
+     */
+    suspend fun distinguishAsModPost(id: String, distinguish: Boolean) : ApiResponse<RedditPost> {
+        try {
+            Util.verifyLoggedInToken(accessToken)
+        } catch (e: InvalidAccessTokenException) {
+            return ApiResponse.Error(GenericError(-1), InvalidAccessTokenException("Distinguishing a post requires a valid access token for a logged in user", e))
+        }
+
+        return try {
+            val resp = api.distinguishAsModPost(
+                    Util.createFullName(Thing.POST, id),
+                    if (distinguish) "yes" else "no",
+                    RedditApi.API_TYPE
+            )
+
+            val post = resp.body()?.response?.getListings()?.get(0)
+            if (post != null) {
+                ApiResponse.Success(post)
+            } else {
+                apiError(resp)
+            }
+        } catch (e: Exception) {
+            ApiResponse.Error(GenericError(-1), e)
+        }
+    }
+
+
+    /**
+     * Sticky or unsticky a post
+     *
+     * @param id The id of the post
+     * @param sticky True to sticky, false to unsticky. If the post is already stickied and this is true,
+     *               a 409 Conflict error will occur
+     * @return No data is returned
+     */
+    suspend fun stickyPost(id: String, sticky: Boolean) : ApiResponse<Nothing?> {
+        try {
+            Util.verifyLoggedInToken(accessToken)
+        } catch (e: InvalidAccessTokenException) {
+            return ApiResponse.Error(GenericError(-1), InvalidAccessTokenException("Stickying a post requires a valid access token for a logged in user", e))
+        }
+
+        return try {
+            val resp = api.stickyPost(
+                    Util.createFullName(Thing.POST, id),
+                    sticky,
+                    RedditApi.API_TYPE
+            )
+
+            if (resp.isSuccessful) {
+                ApiResponse.Success(null)
+            } else {
+                apiError(resp)
+            }
+        } catch (e: Exception) {
+            ApiResponse.Error(GenericError(-1), e)
+        }
+    }
+
+}
---
+++ app/app/src/main/java/com/example/hakonsreader/api/service/ModServiceKt.kt
@@ -0,0 +1,79 @@
+package com.example.hakonsreader.api.service
+
+import com.example.hakonsreader.api.model.RedditComment
+import com.example.hakonsreader.api.model.RedditPost
+import com.example.hakonsreader.api.responses.JsonResponseKt
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+/**
+ * Interface for making mod related API requests
+ */
+interface ModServiceKt {
+
+    // Comments and posts work a bit differently. If comments are stickied, they MUST also be distinguished as mod
+    // Posts can be stickied WITHOUT being distinguished as mod, and they use a different api call to sticky
+    // Distinguishing a post also cannot send a "sticky" parameter, or else it will fail with 400 Bad Request, so
+    // we need two different functions for comments and post distinguishing
+
+
+    /**
+     * Distinguish a comment as a moderator
+     *
+     * OAuth scope required: *modposts*
+     *
+     * @param fullname The fullname of the comment
+     * @param how "yes" to distinguish as mod, "no" to remove the mod distinguish
+     * @param sticky True to also sticky the comment (comments will also be mod distinguished when stickied)
+     * @param apiType The string "json"
+     * @return A Response with a [JsonResponseKt]. The response will hold the updated comment
+     */
+    @POST("api/distinguish")
+    @FormUrlEncoded
+    suspend fun distinguishAsModComment(
+            @Field("id") fullname: String,
+            @Field("how") how: String,
+            @Field("sticky") sticky: Boolean,
+            @Field("api_type") apiType: String
+    ) : Response<JsonResponseKt<RedditComment>>
+
+
+    /**
+     * Distinguish a post as a moderator
+     *
+     * OAuth scope required: *modposts*
+     *
+     * @param fullname The fullname of the post
+     * @param how "yes" to distinguish as mod, "no" to remove the mod distinguish
+     * @param apiType The string "json"
+     * @return A Response with a [JsonResponseKt]. The response will hold the updated post
+     */
+    @POST("api/distinguish")
+    @FormUrlEncoded
+    suspend fun distinguishAsModPost(
+            @Field("id") fullname: String,
+            @Field("how") how: String,
+            @Field("api_type") apiType: String
+    ) : Response<JsonResponseKt<RedditPost>>
+
+    /**
+     * Sticky a post on the subreddit
+     *
+     * OAuth scope required: *modposts*
+     *
+     * @param fullname The fullname of the post
+     * @param sticky True to sticky the post
+     * @param apiType The string "json"
+     * @return The response will not hold any data, but potential errors will be handled by the
+     * [JsonResponseKt]
+     */
+    @POST("api/set_subreddit_sticky")
+    @FormUrlEncoded
+    suspend fun stickyPost(
+            @Field("id") fullname: String,
+            @Field("state") sticky: Boolean,
+            @Field("api_type") apiType: String
+    ) : Response<JsonResponseKt<Nothing>>
+}
