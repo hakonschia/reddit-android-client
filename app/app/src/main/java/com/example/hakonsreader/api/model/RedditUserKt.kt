05f71dca4967ec778c988a8ed257181264c541d9
Håkon Schia <hakonschia5@gmail.com>
2020-11-27 09:45:25 +0100

    Add Kotlin version of RedditUser

---
+++ app/app/src/main/java/com/example/hakonsreader/api/model/RedditUserKt.kt
@@ -0,0 +1,55 @@
+package com.example.hakonsreader.api.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Class representing a Reddit user
+ */
+class RedditUserKt : RedditListingKt() {
+
+    /**
+     * The amount of comment karma the user has
+     */
+    @SerializedName("comment_karma")
+    var commentKarma = 0
+
+    /**
+     * The amount of post/link karma the user has
+     */
+    @SerializedName("link_karma")
+    var postKarma = 0
+
+    /**
+     * The amount of awardee karma the user has
+     */
+    @SerializedName("awardee_karma")
+    var awardeeKarma = 0
+
+    /**
+     * The amount of awarder karma the user has
+     */
+    @SerializedName("awarder_karma")
+    var awarderKarma = 0
+
+    /**
+     * The combined total amount of karma the user has
+     */
+    @SerializedName("total_karma")
+    var totalKarma = 0
+
+
+    /**
+     * The URL pointing to the users profile picture
+     */
+    @SerializedName("icon_img")
+    var profilePicture = ""
+
+    /**
+     * True if the user is an admin (employee) at Reddit
+     */
+    @SerializedName("is_employee")
+    var isAdmin = false
+
+
+    // TODO there is also an inner "subreddit" object which is somewhat identical to the "Subreddit" class
+}
