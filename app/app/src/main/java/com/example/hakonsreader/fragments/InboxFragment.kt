8ff9ce728b3ec50e84a3e4342efea1734a7b1282
Håkon Schia <hakonschia5@gmail.com>
2020-12-12 12:25:44 +0100

    Add shell for showing inbox

---
+++ app/app/src/main/java/com/example/hakonsreader/fragments/InboxFragment.kt
@@ -0,0 +1,23 @@
+package com.example.hakonsreader.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Fragment for displaying a users inbox
+ */
+class InboxFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+}
