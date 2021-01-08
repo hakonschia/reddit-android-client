2f5d79bc6fba999011739d28ad3fc934f74741f1
Håkon Schia <hakonschia5@gmail.com>
2021-01-08 12:12:33 +0100

    Show timestamp when a comment was edited

--- app/app/src/main/java/com/example/hakonsreader/views/util/BindingAdapters.java
+++ app/app/src/main/java/com/example/hakonsreader/views/util/BindingAdapters.java
@@ -43,6 +43,25 @@ public class BindingAdapters {
     }
 
     /**
+     * Binding adapter for setting the age text on the post. The text is formatted as "*2 hours", "*1 day" etc.
+     * For a shortened text (*5m etc.) use {@link BindingAdapters#setAgeTextEditedShortened(TextView, long)}
+     *
+     * @param textView The textView to set the text on
+     * @param createdAt The timestamp the post was created at. If this is negative nothing is done
+     */
+    @BindingAdapter({"editedAt"})
+    public static void setAgeTextEdited(TextView textView, long createdAt) {
+        if (createdAt >= 0) {
+            Instant created = Instant.ofEpochSecond(createdAt);
+            Instant now = Instant.now();
+            Duration between = Duration.between(created, now);
+
+            textView.setText("*" + Util.createAgeText(textView.getResources(), between));
+        }
+    }
+
+
+    /**
      * Binding adapter for setting the age text on the post. The text is formatted as "2h", "1d" etc..
      * For a longer text (5 minutes etc.) use {@link BindingAdapters#setAgeText(TextView, long)}
      *
@@ -60,6 +79,23 @@ public class BindingAdapters {
         }
     }
 
+    /**
+     * Binding adapter for setting the time when a post/comment was edited. The text is formatted as "*2h", "*1d" etc..
+     * For a longer text (5 minutes etc.) use {@link BindingAdapters#setAgeTextEdited(TextView, long)} (TextView, long)}
+     *
+     * @param textView The textView to set the text on
+     * @param createdAt The timestamp the post was created at. If this is negative nothing is done
+     */
+    @BindingAdapter({"editedAtShortened"})
+    public static void setAgeTextEditedShortened(TextView textView, long createdAt) {
+        if (createdAt >= 0) {
+            Instant created = Instant.ofEpochSecond(createdAt);
+            Instant now = Instant.now();
+            Duration between = Duration.between(created, now);
+
+            textView.setText("*" + Util.createAgeTextShortened(textView.getResources(), between));
+        }
+    }
 
     /**
      * Sets a TextView with markdown text.
