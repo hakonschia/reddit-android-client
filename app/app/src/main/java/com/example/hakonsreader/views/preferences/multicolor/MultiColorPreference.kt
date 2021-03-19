7c8f72fc0c4371149c53e2f1c8d65cba6162568f
Håkon Schia <hakonschia5@gmail.com>
2021-03-19 13:24:42 +0100

    Add summary for MultiColorPreference

--- app/app/src/main/java/com/example/hakonsreader/views/preferences/multicolor/MultiColorPreference.kt
+++ app/app/src/main/java/com/example/hakonsreader/views/preferences/multicolor/MultiColorPreference.kt
@@ -6,5 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.preference.DialogPreference
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hakonsreader.R
 
-class MultiColorPreference(context: Context, attributeSet: AttributeSet) : DialogPreference(context, attributeSet)
+class MultiColorPreference(context: Context, attributeSet: AttributeSet) : DialogPreference(context, attributeSet) {
+    override fun getSummary(): CharSequence {
+        val colorCount = MultiColorFragCompat.getColors(sharedPreferences, key).size
+        return context.resources.getQuantityString(R.plurals.multiColorPreferenceSummary, colorCount, colorCount)
+    }
+}
