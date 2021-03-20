06f9fdc1763cc770010533a32638e8fe3ddabad3
Håkon Schia <hakonschia5@gmail.com>
2021-03-20 09:38:37 +0100

    Add inferred URL to peek URL bottom sheet when URL is to internal Reddit page

--- app/app/src/main/java/com/example/hakonsreader/fragments/bottomsheets/PeekUrlBottomSheet.kt
+++ app/app/src/main/java/com/example/hakonsreader/fragments/bottomsheets/PeekUrlBottomSheet.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.hakonsreader.activities.DispatcherActivity
 import com.example.hakonsreader.databinding.BottomSheetPeekUrlBinding
+import com.example.hakonsreader.misc.CreateIntentOptions
+import com.example.hakonsreader.misc.createIntent
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class PeekUrlBottomSheet : BottomSheetDialogFragment() {
@@ -16,9 +18,15 @@ class PeekUrlBottomSheet : BottomSheetDialogFragment() {
         private const val TEXT_KEY = "text"
         private const val URL_KEY = "url"
 
+        /**
+         * Creates a new bottom sheet
+         *
+         * @param text The text of the URL. This will be trimmed
+         * @param url The URL
+         */
         fun newInstance(text: String, url: String) = PeekUrlBottomSheet().apply {
             arguments = Bundle().apply {
-                putString(TEXT_KEY, text)
+                putString(TEXT_KEY, text.trim())
                 putString(URL_KEY, url)
             }
         }
@@ -35,25 +43,29 @@ class PeekUrlBottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val text = requireArguments().getString(TEXT_KEY)
-        val url = requireArguments().getString(URL_KEY)
+        val url = requireArguments().getString(URL_KEY) ?: return
 
         val openLinkListener = View.OnClickListener {
-            if (url.isNullOrEmpty()) {
-                return@OnClickListener
-            }
-
             Intent(context, DispatcherActivity::class.java).apply {
                 putExtra(DispatcherActivity.URL_KEY, url)
                 requireContext().startActivity(this)
             }
         }
 
-        binding.openLink.setOnClickListener(openLinkListener)
-        binding.url.setOnClickListener(openLinkListener)
-
         binding.text.text = text
         binding.url.text = url
+
+        // URLs sent here might be of "/r/whatever", so assume those are links to within reddit.com
+        if (!url.matches("^http(s)?.*".toRegex())) {
+            binding.inferredUrlValue = "https://reddit.com" + (if (url[0] == '/') "" else "/") + url
+        }
+
+        binding.openLink.setOnClickListener(openLinkListener)
+        binding.url.setOnClickListener(openLinkListener)
+        binding.inferredUrl.setOnClickListener(openLinkListener)
+
         binding.url.paintFlags = binding.url.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        binding.inferredUrl.paintFlags = binding.url.paintFlags or Paint.UNDERLINE_TEXT_FLAG
     }
 
     override fun onDestroyView() {
